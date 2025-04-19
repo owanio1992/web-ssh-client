687775b20c89771e5163fefa921f9d8201e77074
owan <chihhao.wang@zyxel.com.tw>
2025-04-19 21:52:33 +0800

    adjust logger

--- be/be/settings.py
+++ be/be/settings.py
@@ -189,9 +189,16 @@ STATIC_ROOT = path.join(BASE_DIR, 'static', )
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s [%(levelname)s] [%(name)s] %(message)s'
+        },
+    },
     'handlers': {
         'console': {
+            'level': 'INFO',
             'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
         },
     },
     'loggers': {
