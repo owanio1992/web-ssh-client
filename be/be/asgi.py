903a22cd55b66830f966265375fb3e78111ffcfa
owan <chihhao.wang@zyxel.com.tw>
2025-04-19 18:59:52 +0800

    be asgi

--- be/be/asgi.py
+++ be/be/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for be project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
+import django
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'be.settings')
+django.setup()
 
-application = get_asgi_application()
+from . import routing
+print("asgi.py load")
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
